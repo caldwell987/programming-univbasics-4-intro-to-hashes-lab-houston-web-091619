56e9730820cdc4e7d20ffe274174db9212eaba90
David Caldwell <david@avvitt.com>
2019-09-15 01:36:56 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -30,13 +30,9 @@ def my_hash_creator(key, value)
 end
 
 def read_from_hash(hash, key)
-   sup = {
-    :name => { 
-      (hash)=> { 
-      (key)
-      }
-    }
-   }
+sup = {
+    (hash) => (key)
+  }
 end
 
 def update_counting_hash(hash, key)
